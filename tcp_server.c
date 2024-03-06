2a8b0dbe95b5fcd90c7e976a15af8188485d5319
marcus <marcus.asberg.98@gmail.com>
2024-03-06 22:05:20 +0100

    added more functions with very long names......

--- tcp_server.c
+++ tcp_server.c
@@ -1,7 +1,24 @@
 #include "socket.h"
 #include <stdbool.h>
+#include <pthread.h>
 
 #define MAX_BUFFER_SIZE 256
+
+struct Accepted_sockets
+{
+        int accepted_sockets_fd;
+        struct sockaddr_in address;
+        int error;
+        bool accpted_successfully;
+};
+
+struct Accepted_sockets *accept_incoming_connections(int server_socket_fd);
+
+void receive_and_display(int socketfd);
+
+void start_accept_incoming_Connections(int server_socket_fd);
+
+void accept_new_connetions_receive_n_display(int server_socket_fd);
 
 int main(void)
 {
@@ -33,36 +50,69 @@ int main(void)
                 perror("Listen failed");
         }
 
+        struct Accepted_sockets *client_socket_fd = accept_incoming_connections(server_socket_fd);
+
+        receive_and_display(client_socket_fd->accepted_sockets_fd);
+
+        // Close the socket
+        close(client_socket_fd->accepted_sockets_fd);
+        shutdown(server_socket_fd, SHUT_RDWR);
+
+        return 0;
+}
+
+struct Accepted_sockets *accept_incoming_connections(int server_socket_fd)
+{
         struct sockaddr_in clientAdress;
         int clientAdressSize = sizeof(struct sockaddr_in);
         int client_socket_fd = accept(server_socket_fd, (struct sockaddr *)&clientAdress, &clientAdressSize);
 
+        struct Accepted_sockets *Accepted_sockets = malloc(sizeof(struct Accepted_sockets));
+        Accepted_sockets->address = clientAdress;
+        Accepted_sockets->accepted_sockets_fd = client_socket_fd;
+        Accepted_sockets->accpted_successfully = client_socket_fd > 0;
+
+        if (!Accepted_sockets->accpted_successfully)
+        {
+                Accepted_sockets->error = client_socket_fd;
+        }
+
+        return Accepted_sockets;
+}
+
+void receive_and_display(int socketfd)
+{
         char msg_buffer[1024];
 
-        if (client_socket_fd == 0)
+        while (true)
         {
-                printf("Client socket accepted\n");
-        }
-        else
-        {
-                perror("Accept failed");
 
-                while (true)
+                ssize_t amountReceived = recv(socketfd, msg_buffer, 1024, 0);
+                if (amountReceived > 0)
                 {
-                        ssize_t amountReceived = recv(client_socket_fd, msg_buffer, 1024, 0);
-                        if (amountReceived > 0)
-                        {
-                                printf("Client response %s", msg_buffer);
-                        }
-                        if (amountReceived < 0)
-                        {
-                                break;
-                        }
+                        msg_buffer[amountReceived] = 0;
+                        printf("Client response %s", msg_buffer);
                 }
+                if (amountReceived < 0)
+                {
+                        break;
+                }
+        }
+}
 
-                // Close the socket
-                close(server_socket_fd);
+void start_accept_incoming_Connections(int server_socket_fd)
+{
+        pthread_t id;
+        pthread_create(&id, NULL, accept_new_connetions_receive_n_display, server_socket_fd);
+}
 
-                return 0;
+void accept_new_connetions_receive_n_display(int server_socket_fd)
+{
+        while (true)
+        {
+                struct Accepted_sockets *client_socket_fd = accept_incoming_connections(server_socket_fd);
+                receive_and_display(client_socket_fd->accepted_sockets_fd);
+
+                close(client_socket_fd->accepted_sockets_fd);
         }
 }
